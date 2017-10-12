74628315d04c93d5788b3a22fe67d389ba0383f9
JpnLavender <irimamekun@gmail.com>
2017-10-13 00:03:35 +0900

    特定Hotelの検索情報をJsonで返せるように

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require './models'
 
+#hotels
+
+get '/hotel/:id' do
+  return Hotel.find_by(id: params["id"]).to_json
+end
+
 get '/hotels/:search' do
   response.headers['Access-Control-Allow-Origin'] = '*'
   return Hotel.search(params["search"].chomp).to_json
